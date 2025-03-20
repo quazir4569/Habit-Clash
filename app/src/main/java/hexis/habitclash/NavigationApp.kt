d96afea7a8e49c6ddd4f5d8375a1c43ef5c4b6f4
Vincent <ademv@sheridancollege.ca>
2025-03-19 22:40:35 -0400

    -I added a navigationApp to link all of the Screens/Pages -I added the Firebase that handles our email and password -I added a TestHomeScreen to make sure it is function correctly

--- app/src/main/java/hexis/habitclash/NavigationApp.kt
+++ app/src/main/java/hexis/habitclash/NavigationApp.kt
@@ -7,7 +7,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun NavigationApp(modifier: Modifier = Modifier, authViewModel: AuthViewModel){
+fun NavigationApp(authViewModel: AuthViewModel){
 
     val navController = rememberNavController()
 
